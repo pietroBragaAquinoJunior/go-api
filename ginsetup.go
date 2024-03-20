ed9317093cc10c29c1f5dbb882c03371b4321c61
pietro <pietro.aquino@tce.ma.gov.br>
2024-03-20 10:04:13 -0300

    refatorando métodos gin

--- ginsetup.go
+++ ginsetup.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 func ginSetup(db *gorm.DB, pindex *ProviderIndex) *gin.Engine {
@@ -20,9 +21,6 @@ func ginSetup(db *gorm.DB, pindex *ProviderIndex) *gin.Engine {
 	// JWT
 	router.POST("/login", func(c *gin.Context) { login(c, db) })
 
-
-
-
 	//OAUTH
 	router.GET("/auth/callback", func(c *gin.Context) { providerCallback(c) })
 	router.GET("/logout", func(c *gin.Context) { oauthLogout(c) })
@@ -30,4 +28,31 @@ func ginSetup(db *gorm.DB, pindex *ProviderIndex) *gin.Engine {
 	router.GET("/", func(c *gin.Context) { getTemplate(c, pindex) })
 
 	return router
+}
+
+
+func getAlbums(c *gin.Context, db *gorm.DB) {
+	var albums []Album
+	db.Find(&albums)
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbum(c *gin.Context, db *gorm.DB) {
+	var album Album
+	if err := c.BindJSON(&album); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Create(&album)
+	c.JSON(http.StatusOK, album)
+}
+
+func getAlbumByID(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	var album Album
+	if err := db.First(&album, id).Error; err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": "Album não encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, album)
 }
--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
 	"github.com/markbates/goth/gothic"
-	"gorm.io/gorm"
 )
 
 func init() {
@@ -30,30 +27,4 @@ func main() {
 	r := ginSetup(db, providerIndex)
 
 	r.Run(":8080")
-}
-
-func getAlbums(c *gin.Context, db *gorm.DB) {
-	var albums []Album
-	db.Find(&albums)
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-func postAlbum(c *gin.Context, db *gorm.DB) {
-	var album Album
-	if err := c.BindJSON(&album); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	db.Create(&album)
-	c.JSON(http.StatusOK, album)
-}
-
-func getAlbumByID(c *gin.Context, db *gorm.DB) {
-	id := c.Param("id")
-	var album Album
-	if err := db.First(&album, id).Error; err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": "Album não encontrado"})
-		return
-	}
-	c.JSON(http.StatusOK, album)
 }
